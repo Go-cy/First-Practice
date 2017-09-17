13287519d2785140659f6ec0e15eda986572026c
Biginus <31850039+Biginus@users.noreply.github.com>
2017-09-17 19:20:59 +0900

    MODIFIED
    2013130002->2013132042
    황샌준->황성준

--- 소스.cpp
+++ 소스.cpp
@@ -8,6 +8,6 @@ void main()
 	cout << "Practice" << endl;
 	cout << "2013130002 고찬윤" << endl;
 	cout << "2013130002 갬대민" << endl;
-	cout << "2013130002 황샌준" << endl;
+	cout << "2013132042 황성준" << endl;
 	cout << "2015130015 박서진" << endl;
 }
