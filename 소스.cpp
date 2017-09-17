201a1aab42ed97b21beceb948b80359d86d0cf26
Go-cy <ckbs24@naver.com>
2017-09-17 17:40:10 +0900

    Initial commit

---
+++ 소스.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+
+	cout << "Practice" << endl;
+	cout << "2013130002 °íÂùÀ±" << endl;
+	cout << "2013130002 °·´ë¹Î" << endl;
+	cout << "2013130002 È²»÷ÁØ" << endl;
+	cout << "2013130002 ¹Ú»õÁø" << endl;
+}
