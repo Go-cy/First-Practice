a28c833a8d56afb538386b7067b12b54da131048
Biginus <31850039+Biginus@users.noreply.github.com>
2017-09-17 18:21:34 +0900

    modified -1
    2013130002->2013132042
    황샌준->황성준

--- 소스.cpp
+++ 소스.cpp
@@ -8,6 +8,6 @@ void main()
 	cout << "Practice" << endl;
 	cout << "2013130002 °íÂùÀ±" << endl;
 	cout << "2013130002 °·´ë¹Î" << endl;
-	cout << "2013130002 È²»÷ÁØ" << endl;
+	cout << "2013132042 È²¼ºÁØ" << endl;
 	cout << "2013130002 ¹Ú»õÁø" << endl;
 }
